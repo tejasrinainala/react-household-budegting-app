da7f54e92c3b16070cc0b9e3124f8bed0c519e4d
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-08-07 20:19:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+import React, { useState } from 'react';
+import './index.css';
+
+function App() {
+    const [showLogin, setShowLogin] = useState(false);
+
+    function login() {
+        setShowLogin(true);
+    }
+
+    return (
+        <div className="ask-user">
+            {showLogin ? (
+                <div>
+                    <input type="text" placeholder='Enter username/number' />
+                    <input type="password" placeholder='Enter password' />
+                </div>
+            ) : (
+                <>
+                    <p>Already a User?</p>
+                    <button onClick={login}>Login</button>
+                    <p>New User?</p>
+                    <button>Sign Up!</button>
+                </>
+            )}
+        </div>
+    );
+}
+
+export default App;
+
 
